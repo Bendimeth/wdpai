890e03881307866f01bc6a220abb305cd58b85e0
Bendimeth <milosz.wodzisz@gmail.com>
2021-06-25 11:56:11 +0200

    merge login-page-init-design to develop

---
+++ public/views/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,500,600' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="public/css/login.css">
+    <title>ActivityLog - login</title>
+</head>
+<body>
+    <div class="login-wrapper">
+        <div class="login-header">
+            <div class="logo-wrapper">
+                <div>
+                    <div class="logo"></div>
+                </div>
+            </div>
+        </div>
+        <div class="login-background"></div>
+        <div class="popup">
+            <div class="popup-header">
+                Sign in to your ActivityLog account
+            </div>
+            <div class="popup-content">
+                <form>
+                    <label for="username">Email</label>
+                    <input type="email" name="username">
+                    <label for="password">Password</label>
+                    <input type="password" name="password">
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
