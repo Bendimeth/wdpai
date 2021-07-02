90b93d8f124809e108d096512c4d46a3dfe5920d
Bendimeth <milosz.wodzisz@gmail.com>
2021-07-02 03:01:33 +0200

    reset config

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME = 'zwhvzubhcaqnqq';
-const PASSWORD = '6de8a06eaab4f6d115a1f8c73d06cd5e23ca263eb0f089227dda2981e8513859';
-const HOST = 'ec2-52-209-134-160.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'd396c52jrn5ouh';
